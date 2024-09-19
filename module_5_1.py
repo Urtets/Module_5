8d944042104fbf1f0827aac455b3d6a90f03a66d
Юрий <urtets@gmail.com>
2024-09-19 10:31:19 +0300

    1 task of Module 5

--- module_5_1.py
+++ module_5_1.py
@@ -15,3 +15,48 @@ class House:
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+
+    def __radd__(self, other):
+        self.number_of_floors += other
+        return self
+
+    def __iadd__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors -= other
+            return self
+
+    def __rsub__(self, other):
+        return other - self.number_of_floors
+
+    def __isub__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors -= other
+            return self
