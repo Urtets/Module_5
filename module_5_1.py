bf3e80cab6d63ca5d29eeb5fc6a3170809118fc2
Юрий <urtets@gmail.com>
2024-09-20 19:35:47 +0300

    task 4 of Module 5

--- module_5_1.py
+++ module_5_1.py
@@ -1,7 +1,18 @@
 class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        for house in args:
+            cls.houses_history.append(house)
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
---
+++ module_5_4.py
@@ -0,0 +1,27 @@
+# class Example:
+#     def __new__(cls, *args, **kwargs):
+#         print(args)
+#         print(kwargs)
+#         return object.__new__(cls)
+#
+#     def __init__(self, first, second, third):
+#         print(first)
+#         print(second)
+#         print(third)
+#
+#
+# ex = Example('data', second=25, third=3.14)
+from module_5_1 import House
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
