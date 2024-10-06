38595a34f6899f58107a0d16c80aed72fffa52cf
Юрий <urtets@gmail.com>
2024-10-06 20:19:40 +0300

    task 4 of Module 5

--- module_5_1.py
+++ module_5_1.py
@@ -49,7 +49,7 @@ class House:
             return self.number_of_floors > other
 
     def __ge__(self, other):
-        return self.number_of_floors >= other
+        return self.__gt__(other) or self.__eq__(other)
 
     def __ne__(self, other):
         return not self.__eq__(other)
